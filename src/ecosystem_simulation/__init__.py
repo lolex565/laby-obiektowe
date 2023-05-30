bbcdbe9659d537cfa4999d536efaa13c5b8b9586
arkregiel <arkregiel@gmail.com>
2023-05-30 16:23:59 +0200

    add random object generation and board printing

--- src/ecosystem_simulation/__init__.py
+++ src/ecosystem_simulation/__init__.py
@@ -1,5 +1,6 @@
 from copy import deepcopy
-from random import randint, choice
+from random import randint, choice, uniform
+import pickle
 
 """
 TODO:
@@ -21,7 +22,7 @@ class Object:
         self.__y = y
         self.__weight = weight
 
-    def get_coordinates(self) -> tuple:
+    def get_position(self) -> tuple:
         """Zwraca współrzędne obiektu"""
         return self.__x, self.__y
     
@@ -154,7 +155,7 @@ class Animal(Object):
         """Zwiększa poziom sytości zwierzęcia o wagę wody"""
         self.__thirst += water.get_weight() % 100
 
-    def can_reproduce_with(self, partner: Animal) -> bool:
+    def can_reproduce_with(self, partner) -> bool:
         """Zwraca True, jeśli zwierzęta mogą się rozmnażać"""
         if type(self).__name__ == type(partner).__name__:
             if self.get_gender() != self.get_gender():
@@ -185,14 +186,14 @@ class Beaver(Animal):
 
     def __init__(self, x: int, y: int, weight: float, speed: int,
                  age: int, thirst: int, satiety: int, gender: bool, view_range: int,
-                 hit_points: int, strenght: int) -> None:
+                 strenght: int, hit_points: int = 100, **kwargs) -> None:
         super().__init__(x, y, weight, speed, age, thirst, satiety, gender, view_range,
-                         hit_points, strenght)
+                         strenght, hit_points)
         
     def eat(self, tree: Tree) -> None:
         """Zjada drzewo"""
         self.__satiety += (tree.get_weight() * tree.get_energy()) % 100
-        self.__teeth_durability -= tree.get_durability() / 100
+        self.__teeth_durability -= tree.get_durability() // 100
 
     def get_teeth_durability(self) -> int:
         """Zwraca trwałość zębów"""
@@ -214,46 +215,52 @@ class Predator(Animal):
 
     def __init__(self, x: int, y: int, weight: float, speed: int,
                  age: int, thirst: int, satiety: int, gender: bool, view_range: int,
-                 hit_points: int, strenght: int, anger: int) -> None:
+                 strenght: int, anger: int, hit_points: int = 100) -> None:
         super().__init__(x, y, weight, speed, age, thirst, satiety, gender, view_range,
-                         hit_points, strenght)
+                         strenght, hit_points)
         self.__anger = anger
 
     def get_anger(self) -> int:
         """Zwraca poziom złości"""
         return self.__anger
     
-    def hunt(self, prey: Prey) -> None:
+    def hunt(self, prey) -> None:
         """Atakuje ofiarę"""
         self.__anger += prey.make_sound()
 
-    def attack(self, prey: Prey) -> None:
+    def attack(self, prey) -> None:
         """Zadaje obrażenia"""
-        prey.set_damage(self.get_strenght() + self.get_weight() + self.get_anger())
+        prey.set_damage(int(self.get_strenght() + self.get_weight() + self.get_anger()))
 
     def make_sound(self) -> int:
-        return self.get_anger() + self.get_weight() + self.get_strenght()
+        return int(self.get_anger() + self.get_weight() + self.get_strenght())
+    
+    def eat(self, prey) -> None:
+        """Zjada ofiarę"""
+        self.__satiety += int(prey.get_weight())
+        self.__satiety += max(self.__satiety, 100)
+        self.__anger -= int(prey.get_weight()) % 100
 
 
 class Prey(Animal):
     def __init__(self, x: int, y: int, weight: float, speed: int,
                  age: int, thirst: int, satiety: int, gender: bool, view_range: int,
-                 hit_points: int, strenght: int, fear: int) -> None:
+                 strenght: int, fear: int, hit_points: int = 100) -> None:
         super().__init__(x, y, weight, speed, age, thirst, satiety, gender, view_range,
-                         hit_points, strenght)
+                         strenght, hit_points)
         self.__fear = fear
 
     def get_fear(self) -> int:
         """Zwraca poziom strachu"""
         return self.__fear
     
-    def run(self, predator: Predator) -> None:
+    def run(self, predator) -> None:
         """Ucieka przed drapieżnikiem"""
         self.__fear += predator.make_sound()
 
-    def defend(self, predator: Predator) -> None:
+    def defend(self, predator) -> None:
         """Obronić się przed drapieżnikiem"""
-        predator.set_damage(self.get_strenght() + self.get_weight() + self.get_fear())
+        predator.set_damage(int(self.get_strenght() + self.get_weight() + self.get_fear()))
 
     def make_sound(self) -> int:
         return self.get_fear() + self.get_weight() + self.get_strenght()
@@ -264,9 +271,9 @@ class Wolf(Predator):
 
     def __init__(self, x: int, y: int, weight: float, speed: int,
                  age: int, thirst: int, satiety: int, gender: bool, view_range: int,
-                 hit_points: int, strenght: int, anger: int) -> None:
+                 strenght: int, anger: int, hit_points: int = 100, **kwargs) -> None:
         super().__init__(x, y, weight, speed, age, thirst, satiety, gender, view_range,
-                         hit_points, strenght, anger)
+                         strenght, anger, hit_points)
         
     def roar(self) -> int:
         """Wydać dźwięk"""
@@ -294,7 +301,7 @@ class Eagle(Predator):
 
     def __init__(self, x: int, y: int, weight: float, speed: int,
                  age: int, thirst: int, satiety: int, gender: bool, view_range: int,
-                 hit_points: int, strenght: int, anger: int) -> None:
+                 strenght: int, anger: int, hit_points: int = 100, **kwargs) -> None:
         super().__init__(x, y, weight, speed, age, thirst, satiety, gender, view_range,
                          hit_points, strenght, anger)
         
@@ -323,9 +330,9 @@ class Mouse(Prey):
 
     def __init__(self, x: int, y: int, weight: float, speed: int,
                  age: int, thirst: int, satiety: int, gender: bool, view_range: int,
-                 hit_points: int, strenght: int, fear: int) -> None:
+                 strenght: int, fear: int, hit_points: int = 100, **kwargs) -> None:
         super().__init__(x, y, weight, speed, age, thirst, satiety, gender, view_range,
-                         hit_points, strenght, fear)
+                         strenght, fear, hit_points)
         
     def squeak(self) -> int:
         """Wydać dźwięk"""
@@ -346,9 +353,9 @@ class Deer(Prey):
 
     def __init__(self, x: int, y: int, weight: float, speed: int,
                  age: int, thirst: int, satiety: int, gender: bool, view_range: int,
-                 hit_points: int, strenght: int, fear: int) -> None:
+                 strenght: int, fear: int, hit_points: int = 100, **kwargs) -> None:
         super().__init__(x, y, weight, speed, age, thirst, satiety, gender, view_range,
-                         hit_points, strenght, fear)
+                         strenght, fear, hit_points)
         
     def yell(self) -> int:
         """Wydać dźwięk"""
@@ -393,6 +400,8 @@ class Board:
     
     __total_object_count = 0
     __population = 0
+    __predators = 0
+    __preys = 0
     __grid = []
     __objects = set()
     __round = 0
@@ -417,6 +426,16 @@ class Board:
         self.__size = size
         self.__grid = [[None for _ in range(self.__size.get_size()[0])] for _ in range(self.__size.get_size()[1])]
 
+    def __str__(self) -> str:
+        grid_str = [[str(cell) if cell is not None else " " for cell in row] for row in self.__grid]
+        lengths = [max(map(len, col)) for col in zip(*grid_str)]
+        format = '\t'.join('{{:{}}}'.format(x) for x in lengths)
+        table = [format.format(*row) for row in grid_str]
+        return '\n'.join(table)
+    
+    def __repr__(self) -> str:
+        return self.__str__()
+
     def get_size(self) -> tuple:
         """Zwraca rozmiar planszy"""
         return self.__size.get_size()
@@ -433,6 +452,14 @@ class Board:
         """Zwraca liczbę wszystkich zwierząt"""
         return self.__population
     
+    def get_predators(self) -> int:
+        """Zwraca liczbę drapieżników"""
+        return self.__predators
+    
+    def get_preys(self) -> int:
+        """Zwraca liczbę ofiar"""
+        return self.__preys
+    
     def get_grid(self) -> list:
         """Zwraca planszę"""
         return deepcopy(self.__grid)
@@ -447,15 +474,75 @@ class Board:
     
     def add_random_object(self) -> None:
         """Dodaje losowy obiekt"""
-        pass
+        category = choice("animal", "item")
+        if category == "animal":
+            self.add_random_animal()
+        else:
+            self.add_random_item()
+
+    def __generate_random_position(self) -> tuple:
+        """Generuje losowe współrzędne"""
+        retry = 0
+        while True and retry < 10:
+            x, y = randint(0, self.__size.get_size()[0] -1), randint(0, self.__size.get_size()[1] - 1)
+            if self.__grid[y][x] is None:
+                return (x, y)
+            retry += 1
+        else:
+            return (-1, -1)
+
+    def add_random_animal(self) -> None:
+        """Dodaje losowe zwierzę"""
+        species = choice(list(self.__possible_animals.keys()))
+        
+        x, y = self.__generate_random_position()
+        if x == -1 or y == -1:
+            return # TODO: raise exception
+
+        animal = eval(species)(
+            x=x,
+            y=y,
+            weight=uniform(0.1, 20),
+            speed=randint(1, 20),
+            age=0,
+            thirst=100,
+            satiety=100,
+            gender=choice((GENDER_MALE, GENDER_FEMALE)),
+            view_range=randint(1, 20),
+            strenght=randint(1, 50),
+            fear=randint(1, 10),
+            anger=randint(1, 10),
+            hit_points=100
+        )
+
+        self.__grid[y][x] = animal
+
+    def add_random_item(self) -> None:
+        """Dodaje losowy przedmiot"""
+        category = choice(list(self.__possible_items.keys()))
+
+        x, y = self.__generate_random_position()
+        if x == -1 or y == -1:
+            return # TODO: raise exception
+        
+        item = eval(category)(
+            x=x,
+            y=y,
+            weight=uniform(0.1, 150),
+            energy=randint(1, 10),
+            durability=randint(1, 50),
+        )
+
+        self.__grid[y][x] = item
     
     def populate(self, animal: Animal) -> None:
         """Dodaje zwierzę do planszy"""
         pass
 
-    def remove(self, animal: Animal) -> None:
+    def remove(self, obj) -> None:
         """Usuwa zwierzę z planszy"""
-        pass
+        x, y = obj.get_position()
+        self.__grid[y][x] = None
 
     def update(self) -> None:
         """Aktualizuje stan planszy"""
@@ -464,3 +551,27 @@ class Board:
     def check_end_conditions(self) -> bool:
         """Sprawdza warunki końcowe"""
         pass
+
+
+if __name__ == "__main__":
+    print("Compiles!")
+
+    predator = Wolf(0, 0, 5.6, 20, 3, 100, 100, GENDER_MALE, 10, 50, 20)
+    prey = Mouse(0, 0, 0.1, 10, 1, 100, 100, GENDER_FEMALE, 10, 1, 10)
+    print(predator.get_hit_points())
+    print(prey.get_hit_points())
+    predator.attack(prey)
+    print(prey.get_hit_points())
+    prey.defend(predator)
+    print(predator.get_durability())
+    print(predator.get_hit_points())
+
+    board = Board(BoardSize(10, 10))
+
+    for _ in range(10):
+        board.add_random_animal()
+
+    for _ in range(10):
+        board.add_random_item()
+
+    print(board)
