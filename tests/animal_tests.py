abddb71ae5173da6323c8fe8c6fd88ff8a8b0cde
arkregiel <arkregiel@gmail.com>
2023-06-07 19:53:07 +0200

    add predator eats prey test

--- tests/animal_tests.py
+++ tests/animal_tests.py
@@ -56,6 +56,40 @@ class AnimalTester(unittest.TestCase):
 
         self.assertTrue(hp_after_attack < hp_before_attack, "Ofiara powinna otrzymać obrażenia")
 
+    def test_predator_eats_prey(self):
+        predator = Predator(
+        x=0,
+        y=0,
+        weight=uniform(0.1, 20),
+        speed=randint(1, 5),
+        age=0,
+        thirst=100,
+        satiety=0,
+        hit_points=100,
+        gender=choice((GENDER_MALE, GENDER_FEMALE)),
+        view_range=randint(1, 20),
+        strenght=randint(1, 50),
+        anger=randint(1, 10),
+        )
+        prey = Prey(
+            x=0,
+            y=0,
+            weight=uniform(0.1, 20),
+            speed=randint(1, 5),
+            age=0,
+            thirst=100,
+            satiety=100,
+            hit_points=0,
+            gender=choice((GENDER_MALE, GENDER_FEMALE)),
+            view_range=randint(1, 20),
+            strenght=randint(1, 50),
+            fear=randint(1, 10),
+        )
+
+        predator.eat(prey)
+
+        self.assertTrue(predator.get_satiety() > 0, "Zjedzenie ofiary powinno zwiększyć sytość drapieżnika")
+
 
 if __name__ == '__main__':
     unittest.main()
